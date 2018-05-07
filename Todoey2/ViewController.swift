1c649eede79dad4b6ecc1f6c5a6bfbb30becd981
fblanker <furgill.blanker@gmail.com>
2018-05-07 09:10:16 +0200

    Initial Commit

---
+++ Todoey2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey2
+//
+//  Created by Furgill Blanker on 07/05/2018.
+//  Copyright © 2018 Furgill Blanker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
